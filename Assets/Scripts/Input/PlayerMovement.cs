1a0183f695ef0bcabe883643c3b195a58b03b91c
Emilio <46302975+BloxRollsDeveloper@users.noreply.github.com>
2026-01-07 20:46:03 +0100

    Shiori Movement and working on Gura
    Good progress!! Shiori walking works in theory but there are some things that need to be fixed.

--- Assets/Scripts/Input/PlayerMovement.cs
+++ Assets/Scripts/Input/PlayerMovement.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] private Animator _animator;
+    
     public Rigidbody2D rb;
     public float moveSpeed = 5f;
 
@@ -10,16 +14,34 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-
+        
     }
-
+private void UpdateAnimation()
+    {
+        _animator = GetComponent<Animator>();
+        if (horizontalMovement > 0f)
+        {
+            _animator.SetBool("IsWalking", true);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (horizontalMovement < 0f)
+        {
+            _animator.SetBool("IsWalking", true);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else
+        {
+            _animator.SetBool("IsWalking", false);
+        }
+    }
     private void Update()
     {
+        UpdateAnimation();
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
     }
-
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
     }
+    
 }
