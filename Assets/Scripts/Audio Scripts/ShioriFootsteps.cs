2c1e8b5d7b78a17d9e7a184add54eb154bf4b73c
Emilio <46302975+BloxRollsDeveloper@users.noreply.github.com>
2026-01-09 22:03:58 +0100

    Last Update

---
+++ Assets/Scripts/Audio Scripts/ShioriFootsteps.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// public class ShioriFootsteps : MonoBehaviour
+//{
+    // public AudioClip sfx_pc_stephard_fd1;
+    // private PlayerMovement playerMovement;
+
+    //void Start()
+    //{
+       // playerMovement = GetComponent<PlayerMovement>();
+       // StartCoroutine(PlayShioriFootstep());
+   // }
+
+  //IEnumerator PlayShioriFootstep()
+   // {
+        // while (true)
+       // {
+            // if (playerMovement.playerMovement.magnitude > 0.1f)
+           // {
+                // AudioManager.instance.PlaySFX(sfx_pc_stephard_fd1);
+                //yield return new WaitForSeconds(0.35f);
+            // }
+            // else
+            //{
+                //yield return new WaitForSeconds(0.1f);
+           // }
+        //}
+   // }
+//}
--- Assets/Scripts/Menu/AmbianceController.cs
+++ Assets/Scripts/Menu/AmbianceController.cs
@@ -1,16 +1,22 @@
+using System;
 using UnityEngine;
 
-public class AmbianceController : MonoBehaviour
+public class AmbienceController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public AudioClip ambientClip;
+   public AudioSource audioSource;
+   
+   public static AmbienceController instance;
+   private void Awake()
+   {
+      if (instance == null)
+      {
+         instance = this;
+         DontDestroyOnLoad(gameObject);
+      }
+      else
+      {
+         Destroy(gameObject);
+      }
+   }
 }
