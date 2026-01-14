c6eb9ae214c19a65776260dd49bd9f2a5f58332a
Marmine15 <martinandreas2006@gmail.com>
2026-01-14 19:16:28 +0100

    goooober

--- Assets/Scripts/Input/PlayerTriviaActivation.cs
+++ Assets/Scripts/Input/PlayerTriviaActivation.cs
@@ -5,18 +5,25 @@ public class PlayerTriviaActivation : MonoBehaviour
     private InputBehaviourSystem _input;
     
     public GameObject talentTrivia;
+    public bool isTriviaActive;
 
     private void Start()
     {
         _input = GetComponent<InputBehaviourSystem>();
+        isTriviaActive = false;
     }
 
+    /* this script is a bunch of fucko lines that pisses me off.
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Trivia") && _input.Interact)
+        if (collision.gameObject.CompareTag("Trivia") && !isTriviaActive)
         {
             print("penaur");
-            talentTrivia.SetActive(true);
+            if (_input.Interact)
+            {
+                talentTrivia.SetActive(true);
+                isTriviaActive = true;
+            }
         }
-    }
+    }*/
 }
--- Assets/Scripts/Interactables/Indicator.cs
+++ Assets/Scripts/Interactables/Indicator.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Indicator : MonoBehaviour
 {
     public GameObject starIndicator;
+    public GameObject triviaCanvas;
+    public Button startTrivia;
 
     private void Start()
     {
         if (starIndicator != null)
         {
             starIndicator.SetActive(false);
+        }
+
+        if (startTrivia != null)
+        {
+            startTrivia.gameObject.SetActive(false);
         }
     }
 
@@ -17,6 +25,7 @@ public class Indicator : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             starIndicator.SetActive(true);
+            startTrivia.gameObject.SetActive(true);
         }
     }
 
@@ -25,6 +34,11 @@ public class Indicator : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             starIndicator.SetActive(false);
+            startTrivia.gameObject.SetActive(false);
+            if (triviaCanvas.activeSelf)
+            {
+                triviaCanvas.SetActive(false);
+            }
         }
     }
 }
